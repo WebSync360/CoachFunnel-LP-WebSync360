9072272de305a80515c44354c2b2490b26bfd1ee
Stack Quantum <stackquantum1@gmail.com>
2025-12-07 18:27:30 -0800

    Initial commit with Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // adjust if you have other folders/files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
